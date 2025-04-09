12fcec4a7a415983e2c610a998716d1f81e70d3d
Rapoza <141277901+bairosluizkaick@users.noreply.github.com>
2025-04-09 12:32:49 +0000

    ok

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+//busca de todas as classes .botao e .aba-conteudo
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -13,6 +14,8 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+    //para o cursor parar em um dos botoes da aba o código
+    // ao clicar ele remove a classe .ativa
 }
 
 const contadores = document.querySelectorAll(".contador");
@@ -20,17 +23,20 @@ const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo2 = new Date("2025-10-30T00:00:00");
 const tempoObjetivo3 = new Date("2025-10-16T00:00:00");
 const tempoObjetivo4 = new Date("2028-06-12T00:00:00");
+//tempo do cronometro das metas 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+//listas dos objetivos (simbolo {})
 
 function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
+    let tempoAtual = new Date();//horario atual
+    let tempoFinal = tempoObjetivo - tempoAtual; //diferença de tempo
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+    //calculo de arrendondamento das variaveis de tempo
 
     segundos %= 60;
     minutos %= 60;
